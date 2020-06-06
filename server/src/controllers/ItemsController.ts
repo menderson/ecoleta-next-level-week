90705fcb7bbb446f98a0dc56adac53d8735bfcdc
menderson <menderson07@gmail.com>
2020-06-06 16:02:16 -0300

    Mobile da aplicação

--- server/src/controllers/ItemsController.ts
+++ server/src/controllers/ItemsController.ts
@@ -2,14 +2,15 @@ import { Request, Response } from 'express';
 import knex from '../database/connection';
 
 class ItemsController {
-    async index (request: Request, response: Response) { //index é pradrão para listagem
+    async index(request: Request, response: Response) { //index é pradrão para listagem
         const items = await knex('items').select('*');
 
         const serializedItems = items.map(item => {
             return {
                 id: item.id,
                 title: item.title,
-                image_url: `http://localhost:3333/uploads/${item.image}`,
+                // image_url: `http://localhost:3333/uploads/${item.image}`, //esse nao funciona no expo(mobile)
+                image_url: `http://192.168.0.62:3333/uploads/${item.image}`,
             };
         });
 
